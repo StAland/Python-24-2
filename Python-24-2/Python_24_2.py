17faef412afd362b0d2eb08f860121bd10bf0ca3
StAland <steffen.aland@gmx.de>
2025-07-09 08:31:16 +0200

    Add project files.

---
+++ Python-24-2/Python_24_2.py
@@ -0,0 +1,27 @@
+
+class Zutat:
+    def __init__(self, name):
+        self.__name = name  # Das _ zeigt: "Bitte nicht direkt anfassen"
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, neuer_name):
+        if not neuer_name:
+            raise ValueError("Name darf nicht leer sein.")
+        self.__name = neuer_name
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, neuer_name):
+        if not neuer_name:
+            raise ValueError("Name darf nicht leer sein.")
+        self.__name = neuer_name
+    
+z = Zutat("Tofu")
+print(z.name)
+z.name = "Seitan"
+print(z.name)
